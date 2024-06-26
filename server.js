ea4765b3cef94e4d65ab414e8e99a61a4ba6c346
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-26 10:56:10 +0430

    socket

---
+++ server.js
@@ -0,0 +1,41 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
+
+const PORT = 3000;
+
+server.listen(PORT);
+console.log(`Listening on part ${PORT}`);
+
+let readyPlayerCount = 0;
+
+io.on('connection', (socket)=>{
+    console.log('a user connected', socket.id);
+
+    socket.on('ready', ()=>{
+        console.log('Player ready', socket.id);
+
+        readyPlayerCount++;
+
+        if(readyPlayerCount % 2  === 0){
+            // broadcast("startGame")
+            io.emit('startGame', socket.id)
+        }
+    })
+
+    socket.on('paddleMove', (paddleData)=>{
+        socket.broadcast.emit('paddleMove', paddleData)
+        });
+        
+    socket.on('ballMove', (ballData)=>{
+        socket.broadcast.emit('ballMove', ballData)
+    })
+
+    socket.on('disconnec', (reason)=>{
+        console.log(`Client ${socket.id} disconnected: ${reason}`);
+    })
+});
