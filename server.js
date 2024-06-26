d1914b4a199829c551eda9e38e8577e0b3720234
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-26 16:36:43 +0430

    server js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server, {
+const http = require('http')
+const io = require('socket.io');
+
+const apiServer = require('./api')
+const httpServer = http.createServer(apiServer);
+const sockets = require('./sockets');
+const socketServer = io(httpServer, {
     cors: {
       origin: '*',
       methods: ['GET', 'POST']
@@ -8,34 +13,9 @@ const io = require('socket.io')(server, {
 
 const PORT = 3000;
 
-server.listen(PORT);
+httpServer.listen(PORT);
 console.log(`Listening on part ${PORT}`);
 
-let readyPlayerCount = 0;
+sockets.listen(socketServer)
 
-io.on('connection', (socket)=>{
-    console.log('a user connected', socket.id);
 
-    socket.on('ready', ()=>{
-        console.log('Player ready', socket.id);
-
-        readyPlayerCount++;
-
-        if(readyPlayerCount % 2  === 0){
-            // broadcast("startGame")
-            io.emit('startGame', socket.id)
-        }
-    })
-
-    socket.on('paddleMove', (paddleData)=>{
-        socket.broadcast.emit('paddleMove', paddleData)
-        });
-        
-    socket.on('ballMove', (ballData)=>{
-        socket.broadcast.emit('ballMove', ballData)
-    })
-
-    socket.on('disconnec', (reason)=>{
-        console.log(`Client ${socket.id} disconnected: ${reason}`);
-    })
-});
